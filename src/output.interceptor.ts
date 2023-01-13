9dba5abdea56fac61609ba9dde25cd1f94fd4f9a
MikolajWozniak <wozniak.mikolaj@outlook.com>
2023-01-13 11:03:41 +0000

    sanitize cors

--- src/output.interceptor.ts
+++ src/output.interceptor.ts
@@ -9,20 +9,13 @@ export class OutputInterceptor implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>
   ): Observable<any> | Promise<Observable<any>> {
-    return next.handle().pipe(
-      map((result) => {
-        const res = context.switchToHttp().getResponse();
-        [
-          {'content-type': 'application/json'}, 
-          {'cross-origin-opener-policy': 'unsafe-none'},
-          {'Cross-Origin-Embedder-Policy': 'unsafe-non'},
-          {'Cross-Origin-Resource-Policy': 'cross-origin'}
-        ].forEach(header => res.set(header));
-        res.json(result);
-        return;
-      })
-    );
-    
-    
+    const res = context.switchToHttp().getResponse();
+        res.set({
+          'Content-Type': 'application/json', 
+          'Cross-Origin-Opener-Policy': 'unsafe-none',
+          'Cross-Origin-Embedder-Policy': 'unsafe-non',
+          'Cross-Origin-Resource-Policy': 'cross-origin'
+        });
+    return next.handle();
   }
 }
